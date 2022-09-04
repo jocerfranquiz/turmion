2b5d0ae30a1d34ffaca7962efde4acefc8966173
newpaxonian <newpaxonian@gmail.com>
2022-09-04 02:06:09 -0500

    Cleaning main

--- main.py
+++ main.py
@@ -11,13 +11,7 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode(RES)  # why RES?
         self.clock = pg.time.Clock()
-        # self.running = True
-        # self.state = 'start'
-        # self.load_data()
         self.new_game()
-
-    # def load_data(self):
-    #     pass
 
     def new_game(self):
         self.map = Map(self)
@@ -34,35 +28,18 @@ class Game:
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                # self.running = False
                 pg.quit()
                 sys.exit()
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
-                    # self.running = False
                     pg.quit()
                     sys.exit()
-                # if event.key == pg.K_SPACE:
-                #     self.new_game()
-                #     self.state = 'playing'
-            # if event.type == pg.KEYUP:
-            #     if event.key == pg.K_SPACE:
-            #         self.state = 'start'
 
     def run(self):
-        while True:  # self.running:
+        while True:
             self.check_events()
             self.update()
             self.draw()
-
-            # if self.state == 'start':
-            #     self.draw()
-            # if self.state == 'playing':
-            #     self.update()
-            # if self.state == 'game over':
-            #     self.draw()
-            # if self.state == 'you win':
-            #     self.draw()
 
 
 if __name__ == '__main__':
