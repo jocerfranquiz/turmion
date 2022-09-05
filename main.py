d591ed6fd899b994c09b2d9f12c344ecd52f7e4c
newpaxonian <newpaxonian@gmail.com>
2022-09-04 21:29:09 -0500

    Add ray-casting calculations

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import sys
 from settings import *
 from map import *
 from player import *
+from raycasting import *
 
 
 class Game:
     def __init__(self):
+        self.ray_casting = None
+        self.map = None
+        self.player = None
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
@@ -16,9 +20,11 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.ray_casting = RayCasting(self)
 
     def update(self):
         self.player.update()
+        self.ray_casting.ray_cast()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps():.1f}')
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import math
+
 # game settings
 RES = WIDTH, HEIGHT = 1600, 900
 FPS = 60
@@ -6,3 +8,11 @@ PLAYER_POS = 1.5, 5  # MINI_MAP
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
+
+# ray casting settings
+FOV = math.tau / 6  # FOV = field of view
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20  # max intersections with walls
