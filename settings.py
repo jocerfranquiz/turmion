bf328cd2ef771aa2c7df80e262f42445d911e180
newpaxonian <newpaxonian@gmail.com>
2022-09-05 19:25:00 -0500

    Add mechanics

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ import math
 
 # game settings
 RES = WIDTH, HEIGHT = 1600, 900
+HALF_WIDTH, HALF_HEIGHT = WIDTH // 2, HEIGHT // 2
 FPS = 60
 
 PLAYER_POS = 1.5, 5  # MINI_MAP
@@ -16,3 +17,6 @@ NUM_RAYS = WIDTH // 2
 HALF_NUM_RAYS = NUM_RAYS // 2
 DELTA_ANGLE = FOV / NUM_RAYS
 MAX_DEPTH = 20  # max intersections with walls
+
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)  # distance from player to screen
+SCALE = WIDTH // NUM_RAYS  # scale of the wall
