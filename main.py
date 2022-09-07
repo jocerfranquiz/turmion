4b534bcac46db9929076312f4103c5ef12f9d674
newpaxonian <newpaxonian@gmail.com>
2022-09-07 18:26:30 -0500

    Add weapon sound

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from object_renderer import *
 from sprite_object import *
 from object_handler import *
 from weapon import *
-# from sound import *
+from sound import *
 # from pathfinding import *
 
 
@@ -28,7 +28,7 @@ class Game:
         self.raycasting = RayCasting(self)
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
-        # self.sound = Sound(self)
+        self.sound = Sound(self)
         # self.pathfinding = PathFinding(self)
         # pg.mixer.music.play(-1)
 
