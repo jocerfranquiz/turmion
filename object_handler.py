775de3c15fc19f36ea078a902d1ea843906a685b
newpaxonian <newpaxonian@gmail.com>
2022-09-07 19:02:15 -0500

    Add npc run logic, death animations, sound effects for NPC

--- object_handler.py
+++ object_handler.py
@@ -1,5 +1,5 @@
 from sprite_object import *
-# from npc import *
+from npc import *
 from random import choices, randrange
 
 
@@ -7,12 +7,12 @@ class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
-        # self.npc_list = []
-        # self.npc_sprite_path = 'resources/sprites/npc/'
+        self.npc_list = []
+        self.npc_sprite_path = 'resources/sprites/npc/'
         self.static_sprite_path = 'resources/sprites/static_sprites/'
         self.anim_sprite_path = 'resources/sprites/animated_sprites/'
         add_sprite = self.add_sprite
-        # add_npc = self.add_npc
+        add_npc = self.add_npc
         # self.npc_positions = {}
 
         # spawn npc
@@ -48,6 +48,7 @@ class ObjectHandler:
         add_sprite(AnimatedSprite(game, pos=(1.5, 24.5)))
 
         # npc map
+        add_npc(NPC(game))
         # add_npc(SoldierNPC(game, pos=(11.0, 19.0)))
         # add_npc(SoldierNPC(game, pos=(11.5, 4.5)))
         # add_npc(SoldierNPC(game, pos=(13.5, 6.5)))
@@ -75,11 +76,11 @@ class ObjectHandler:
     def update(self):
         # self.npc_positions = {npc.map_pos for npc in self.npc_list if npc.alive}
         [sprite.update() for sprite in self.sprite_list]
-        # [npc.update() for npc in self.npc_list]
+        [npc.update() for npc in self.npc_list]
         # self.check_win()
 
-    # def add_npc(self, npc):
-    #     self.npc_list.append(npc)
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
